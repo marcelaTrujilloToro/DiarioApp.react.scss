7e60a517cd98674d349091fe8c2186729ea0bd87
marcelaTrujilloToro <marces.ttblue@gmail.com>
2021-07-07 15:29:38 -0500

    first commit

---
+++ src/DiarioApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const DiarioApp = () => {
+    return (
+        <div>
+            <h1>Diario app</h1>
+        </div>
+    )
+};
+export default DiarioApp;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DiarioApp from './DiarioApp';
+import './styles/styles.scss';
+
+ReactDOM.render(
+    <DiarioApp />,
+  
+  document.getElementById('root')
+);
+
